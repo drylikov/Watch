a827e09bd4624f732b67a9cef34bc9eae42f3268
drylikov <denis.rylikov@protonmail.com>
2026-06-04 16:35:06 +0300

    Initial commit: Watch

---
+++ Makefile
@@ -0,0 +1,22 @@
+CFLAGS = -std=c99 -D_POSIX_C_SOURCE=200809L
+CFLAGS += -Wall -pedantic -Wno-parentheses -Ideps
+PREFIX ?= /usr/local
+SRC = src/watch.c
+DEPS = $(wildcard deps/*/*.c)
+OBJS = $(SRC:.c=.o) $(DEPS:.c=.o)
+
+all: watch
+
+watch: $(OBJS)
+	$(CC) $^ $(CFLAGS) -o $@
+
+install: watch
+	install watch $(PREFIX)/bin/watch
+
+uninstall:
+	rm -f $(PREFIX)/bin/watch
+
+clean:
+	rm -f watch $(OBJS)
+
+.PHONY: all clean install uninstall
---
+++ deps/asprintf/asprintf.h
@@ -0,0 +1,28 @@
+#ifndef HAVE_ASPRINTF
+#ifndef ASPRINTF_H
+#define ASPRINTF_H 1
+
+#include <stdarg.h>
+
+/**
+ * Sets `char **' pointer to be a buffer
+ * large enough to hold the formatted string
+ * accepting a `va_list' args of variadic
+ * arguments.
+ */
+
+int
+vasprintf (char **, const char *, va_list);
+
+/**
+ * Sets `char **' pointer to be a buffer
+ * large enough to hold the formatted
+ * string accepting `n' arguments of
+ * variadic arguments.
+ */
+
+int
+asprintf (char **, const char *, ...);
+
+#endif
+#endif
